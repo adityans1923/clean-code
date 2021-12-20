e4cbce5172ed2bb0449b695e01c3220be8f1a9a4
Ashish <juyal.ashish@gmail.com>
2021-12-20 17:55:56 +0530

    test name

--- src/test/java/codesmells/coupler/messagechain/InvoiceTest.java
+++ src/test/java/codesmells/coupler/messagechain/InvoiceTest.java
@@ -10,19 +10,19 @@ public class InvoiceTest {
   Invoice invoice;
 
   @Before public void
-  setUpInvoiceWithShippingCosts() {
+  setup_invoice_with_shipping_costs() {
     invoice = new Invoice(new Customer(new Address(new Country("South Africa", "Africa"))));
   }
 
   @Test public void
-  ShouldReturnRegularPriceOfOneItemWithShippingCost() {
+  Should_return_regular_price_of_one_item_with_shipping_cost() {
     InvoiceItem item = new InvoiceItem(100);
     invoice.addItem(item);
     assertEquals(110, invoice.getTotalPrice(), 0.1);
   }
 
   @Test public void
-  ShouldReturnRegularPriceOfTwoItem() {
+  Should_return_regular_price_of_two_item() {
     InvoiceItem item = new InvoiceItem(100);
     invoice.addItem(item);
     invoice.addItem(item);
